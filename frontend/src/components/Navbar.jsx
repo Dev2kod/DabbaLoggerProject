03f530f18424ecb231f2a24f32a212f2481b39ad
Dev2kod <devesh4coding@gmail.com>
2026-01-05 12:06:39 +0530

    initializing

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full sticky top-0 flex items-center justify-center bg-blue-800 h-24 '>
+        Navbar
+    </div>
+  )
+}
+
+export default Navbar
