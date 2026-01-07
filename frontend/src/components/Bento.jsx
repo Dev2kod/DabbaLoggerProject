615c070904b660c487b3b3022e1980e6090f324a
dev2kod <devesh4coding@gmail.com>
2026-01-07 19:16:37 +0530

    done

--- frontend/src/components/Bento.jsx
+++ frontend/src/components/Bento.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Bento = () => {
   return (
     <section className="h-full flex items-center justify-center bg-slate-800">
-      <div className="max-w-7xl w-full px-6 py-16 text-white">
-        Bento Content
+      <div className="max-w-7xl w-full px-6 py-16 text-white bg-amber-400">
+        
       </div>
     </section>
   );
